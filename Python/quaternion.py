1854cd71df6ce5abba9173fcbd17dfaaea8c6f41
Elmir Dzaka <elmir.dzaka1@gmail.com>
2024-04-22 20:55:52 -0600

    fixed algorithm and added right-sided unit

--- Python/quaternion.py
+++ Python/quaternion.py
@@ -23,15 +23,15 @@ def left_sided_unit(A, q):
     # Initialize structural coefficients and left handed unit
     l = 1
     u = 1
-    Ln = []
     arr = []
     
     # Perform Algorithm
     for d in range(q):
+        Ln = []
         sub1 = d
         sub2 = (1/l) - ((u*d) / l) 
         sub3 = (A[1]/A[3]) * d
-        sub4 = A[0] * ((1-(u*d))/(l*A[2]))
+        sub4 = (A[0] * (1-(u*d)))/(l*A[2])
         Ln.append(sub1)
         Ln.append(sub2)
         Ln.append(sub3)
@@ -44,7 +44,27 @@ def left_sided_unit(A, q):
 
 
 def right_sided_unit(A, q):
-    return False
+    l = 1
+    u = 1
+    arr = []
+
+    # Perform right hand algorithm
+    for d in range(q):
+        Ln = []
+        sub1 = d
+        sub2 = (1/l) - ((u*d) / l) 
+        sub3 =  (A[0] * (1 - (u*d)))/(l*A[3])
+        sub4 = (A[1]/A[2])*d
+        Ln.append(sub1)
+        Ln.append(sub2)
+        Ln.append(sub3)
+        Ln.append(sub4)
+        arr.append(Ln)
+    
+    # Create Quaternion
+    #new_quaternion = Quaternion(arr[0], arr[1], arr[2], arr[3])
+
+    return arr
 
 def main ():
     # Creating a random Quaternions
@@ -76,6 +96,10 @@ def main ():
     # step two: Left sided unit
     Ln = left_sided_unit(A,q)
     print(Ln)
+
+    # Right Sided Unit
+    Rn = right_sided_unit(A,q)
+    print(Rn)
 
 
 if __name__ == "__main__":
